51de9a5b4680330bfa63c3eadaabe3da7964214c
Alex Casillas <alexvcasillas@gmail.com>
2022-02-12 11:13:10 +0100

    fix: add font swap instead of optional

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ export default class Document extends NextDocument {
             crossOrigin='true'
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Marcellus&family=Mulish:wght@300&display=optional'
+            href='https://fonts.googleapis.com/css2?family=Marcellus&family=Mulish:wght@300&display=swap'
             rel='stylesheet'
           />
         </Head>
